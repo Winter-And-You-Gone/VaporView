949056a07fa81542f2e90f355dba9ae29742d67c
Winter-And-You-Gone <Winter-And-You-Gone@users.noreply.github.com>
2026-03-14 20:39:14 +0800

    Size GGA monitor group from content

--- src/RtkConfigDialog.cpp
+++ src/RtkConfigDialog.cpp
@@ -15,7 +15,6 @@
 #include <QRegularExpression>
 #include <QSerialPortInfo>
 #include <QSignalBlocker>
-#include <QSizePolicy>
 #include <QTextBlock>
 #include <QTextCursor>
 #include <cmath>
@@ -245,7 +244,6 @@ void RtkConfigDialog::setupUi()
     main_layout_->addWidget(output_group_);
 
     gga_group_ = new QGroupBox(this);
-    gga_group_->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Minimum);
     gga_layout_ = new QVBoxLayout(gga_group_);
     gga_layout_->setSpacing(6);
     gga_layout_->setContentsMargins(8, 8, 8, 12);
@@ -462,11 +460,16 @@ void RtkConfigDialog::applyScaledUiMetrics()
     gga_port_combo_->setMinimumHeight(scalePixels(30));
 
     gga_status_label_->setMinimumHeight(scalePixels(24));
-    const int ggaTextHeight = scalePixels(150);
-    gga_text_edit_->setMinimumHeight(ggaTextHeight);
-    gga_text_edit_->setMaximumHeight(ggaTextHeight);
+    const int ggaTextHeight = scalePixels(140);
+    gga_text_edit_->setFixedHeight(ggaTextHeight);
     gga_text_edit_->document()->setDocumentMargin(scalePixels(12));
-    gga_group_->setMinimumHeight(ggaTextHeight + scalePixels(92));
+    gga_group_->setMinimumHeight(0);
+    gga_group_->setMaximumHeight(QWIDGETSIZE_MAX);
+    if (gga_layout_)
+    {
+        gga_layout_->activate();
+    }
+    gga_group_->setFixedHeight(gga_group_->sizeHint().height());
 
     applyButtonWidth(refresh_ports_btn_, 80);
     applyButtonWidth(fetch_mountpoints_btn_, 128);
