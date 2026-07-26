865955c336284b8a910a3f90633605d1bfd4ae45
Winter <2986553814@qq.com>
2026-07-26 20:30:44 +0800

    Lighten top-level card shadows

--- src/shared/theme/TopLevelCardStyle.h
+++ src/shared/theme/TopLevelCardStyle.h
@@ -13,7 +13,7 @@ inline constexpr int kTopLevelCardCornerRadius = 12;
 inline constexpr qreal kTopLevelCardShadowBlurRadius = 6.0;
 inline constexpr qreal kTopLevelCardShadowOffsetY = 2.0;
 inline constexpr qreal kTopLevelCardShadowVerticalSpreadExtra = 2.0;
-inline constexpr int kTopLevelCardShadowAlpha = 18;
+inline constexpr int kTopLevelCardShadowAlpha = 12;
 
 void configureTopLevelCard(QWidget *card);
 void updateTopLevelCardShadows(QWidget *scope, qreal uiScale = 1.0);
