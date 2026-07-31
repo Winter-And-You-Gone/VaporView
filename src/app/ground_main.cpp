d0e3375b05dcdbf59e1bc55f90c946fda5f57530
Winter <2986553814@qq.com>
2026-07-31 21:25:50 +0800

    Release VaporView 1.0.13

--- src/app/ground_main.cpp
+++ src/app/ground_main.cpp
@@ -176,7 +176,7 @@ int main(int argc, char *argv[])
     app.installEventFilter(&wheelValueChangeFilter);
 
     app.setApplicationName("VaporView");
-    app.setApplicationVersion("1.0.12");
+    app.setApplicationVersion("1.0.13");
     app.setOrganizationName("VaporView");
     VaporView::LogService logService(QStringLiteral("VaporView"));
     logService.installQtMessageHandler();
